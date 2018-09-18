2bd3a4720a1dd624aa16be872edadf058c7230d1
Elizabeth Hanley <hanley@Bingen.local>
2018-09-17 22:55:43 -0400

    Created Python file and uploaded json training file.

--- Recommender_System.py
+++ Recommender_System.py
@@ -1,6 +1,20 @@
-import numpy as np
 import pandas as pd
-import gzip
+import json
 
-training_data = pd.read_json(gzip.open('reviews.training.json.gz'))
-print(training_data.head())
+def get_training_data(file_name):
+    try:
+        with open(file_name) as json_file:
+            data = {'data': [json.loads(line) for line in json_file]}
+        return data
+    except:
+        print('Please try another file name.')
+        return None
+
+# training_data = get_training_data('reviews.training.json')
+shortened_training_data = get_training_data('shortened_training_data.json')
+
+
+# with open('shortened_training_data.json', 'w') as outfile:
+#     json.dump(training_data['data'][0:100], outfile)
+
+print(shortened_training_data['data'])
