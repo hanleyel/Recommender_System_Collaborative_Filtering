987a3905f95bdacf2c414a54cef6111f7a66fc09
Elizabeth Hanley <hanley@Bingen.local>
2018-09-29 20:19:24 -0400

    Implemented SVD matrix.

--- Recommender_System.py
+++ Recommender_System.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import json
+import gzip
 import numpy as np
 from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import svds
 from sklearn.neighbors import NearestNeighbors
+
+###################################
+#####     UNZIP JSON DATA     #####
+###################################
+
+# def unzip_json(filename):
+#     unzipped_data = pd.read_json(gzip.open(filename))
+#     return unzipped_data
 
 
 ############################################
@@ -13,11 +23,8 @@ from sklearn.neighbors import NearestNeighbors
 def get_training_data(file_name):
 
     try:
-        with open(file_name) as json_file:
-            data = {'data': json.loads(line) for line in json_file}
-            data_frame = pd.DataFrame(data['data'], columns=['reviewerID', 'asin', 'reviewerName', 'helpful',
-                             'reviewText', 'overall', 'summary', 'unixReviewTime', 'reviewTime'])
-            return data_frame
+        training_data = pd.read_json(file_name, lines=True)
+        return training_data.head(10000)
     except:
         print('Please try another file name.')
         return None
@@ -33,6 +40,11 @@ def convert_to_csv(dataframe, desired_filename):
     except:
         print('Please try another dataframe or file name.')
 
+#################################################################
+#####     ALTERNATE LOADING AND STORING DATA WITH IJSON     #####
+#################################################################
+
+# Using ijson is a way to read the data as a "stream", which is better for scaling up to large datasets
 
 ########################################################
 #####     CONVERTING DATAFRAME TO A CSR MATRIX     #####
@@ -43,7 +55,7 @@ def create_reviewer_product_matrix(dataframe):
 
     # Pivot the dataframe so that unique reviewers are on the y axis and unique products are on the x axis.
     # NOTE: Removed zeros in order to perform algebraic operations.
-    reviewer_product_dataframe = dataframe.pivot(index='reviewerID', columns='asin', values='overall').fillna(0)
+    reviewer_product_dataframe = dataframe.pivot(index='asin', columns='reviewerID', values='overall').fillna(0)
 
     # Convert the dataframe to a matrix.
     # This matrix still contains NaN values.
@@ -69,42 +81,67 @@ def k_nn(matrix):
         return None
 
 
-##########################################
-#####     MAKING RECOMMENDATIONS     #####
-##########################################
-
+# Make k Nearest Neighbors recommendations
 def make_recommendations(dataframe):
     query_index = np.random.choice(dataframe.shape[0])
     distances, indices = model_knn.kneighbors(dataframe.iloc[query_index, :].values.reshape(1, -1), n_neighbors = 6)
     for i in range(0, len(distances.flatten())):
         if i == 0:
-            print('Nearest neighbors of {0}:\n'.format(dataframe.index[query_index]))
+            print('Items most similar to {0}:\n'.format(dataframe.index[query_index]))
         else:
             print('{0}: {1}, with distance of {2}:'.format(i, dataframe.index[indices.flatten()[i]], distances
                                                            .flatten()[i]))
     return None
 
 
+###########################################
+#####     IMPLEMENTING SVD MATRIX     #####
+###########################################
+
+# De-mean the data
+def de_mean(reviewer_product_dataframe):
+    rp_matrix = reviewer_product_dataframe.values
+    user_ratings_mean = np.mean(rp_matrix, axis = 1)
+    r_de_meaned = rp_matrix - user_ratings_mean.reshape(-1, 1)
+
+    return r_de_meaned
+
+def create_svd(de_meaned_matrix):
+    U, sigma, Vt = svds(de_meaned_matrix, k=50)
+    return U, sigma, Vt
+
+def to_diagonal_matrix(sigma):
+    sigma = np.diag(sigma)
+    return sigma
+
 
 #####################################
 #####     RUNNING FUNCTIONS     #####
 #####################################
 
-training_data = get_training_data('shortened_training_data.json')
+# unzipped_data = unzip_json('reviews.training.json.gz')
+training_data = get_training_data('reviews.training.json')
 # convert_to_csv(shortened_training_data, 'shortened_training_data.csv')
-reviewer_product_sparse = create_reviewer_product_matrix(training_data)[0]
-reviewer_product_dataframe = create_reviewer_product_matrix(training_data)[1]
-model_knn = k_nn(reviewer_product_sparse)
-make_recommendations(reviewer_product_dataframe)
-
+reviewer_product_matrix = create_reviewer_product_matrix(training_data)
+reviewer_product_sparse = reviewer_product_matrix[0]
+reviewer_product_dataframe = reviewer_product_matrix[1]
+# model_knn = k_nn(reviewer_product_sparse)
+# make_recommendations(reviewer_product_dataframe)
+de_meaned_matrix = de_mean(reviewer_product_matrix[1])
+rp_svd = create_svd(de_meaned_matrix)
+diag_matrix = to_diagonal_matrix(rp_svd[1])
 
 ####################################
 #####     PRINT STATEMENTS     #####
 ####################################
 
+# print(training_data)
 # print(shortened_training_data[['reviewerID', 'asin', 'reviewerName', 'helpful', 'overall', 'summary']].head())
 # print(shortened_training_data['reviewerID'].value_counts())
 # print(shortened_training_data['asin'].value_counts())
-# print(reviewer_product_matrix)
+# print(reviewer_product_sparse)
 # print(reviewer_product_dataframe)
 # print(model_knn)
+print(de_meaned_matrix)
+print(rp_svd)
+print(diag_matrix)
